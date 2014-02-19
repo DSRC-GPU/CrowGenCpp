f8dc43716cbe9d3d63d780d11493963b2f4993f6
Jesse Donkervliet <j.j.r.donkervliet@student.tudelft.nl>
2014-02-19 17:19:21 +0100

    Added static movement vectors.

---
+++ directions.cpp
@@ -0,0 +1,24 @@
+
+#include "directions.hpp"
+
+namespace directions
+{
+  vector<int> DIRS_1_X = { -1, 0 };
+  vector<int> DIRS_2_X = { -1, 0, 1 };
+  vector<int> DIRS_3_X = { 0, 1 };
+  vector<int> DIRS_4_X = { -1, 0 };
+  vector<int> DIRS_5_X = { -1, 0, 1 };
+  vector<int> DIRS_6_X = { 0, 1 };
+  vector<int> DIRS_7_X = { -1, 0 };
+  vector<int> DIRS_8_X = { -1, 0, 1 };
+  vector<int> DIRS_9_X = { 0, 1 };
+  vector<int> DIRS_1_Y = { -1, 0 };
+  vector<int> DIRS_2_Y = { -1, 0 };
+  vector<int> DIRS_3_Y = { -1, 0 };
+  vector<int> DIRS_4_Y = { -1, 0, 1 };
+  vector<int> DIRS_5_Y = { -1, 0, 1 };
+  vector<int> DIRS_6_Y = { -1, 0, 1 };
+  vector<int> DIRS_7_Y = { 1, 0 };
+  vector<int> DIRS_8_Y = { 1, 0 };
+  vector<int> DIRS_9_Y = { 1, 0 };
+}
---
+++ directions.hpp
@@ -0,0 +1,22 @@
+
+namespace directions
+{
+  extern vector<int> DIRS_1_X;
+  extern vector<int> DIRS_2_X;
+  extern vector<int> DIRS_3_X;
+  extern vector<int> DIRS_4_X;
+  extern vector<int> DIRS_5_X;
+  extern vector<int> DIRS_6_X;
+  extern vector<int> DIRS_7_X;
+  extern vector<int> DIRS_8_X;
+  extern vector<int> DIRS_9_X;
+  extern vector<int> DIRS_1_Y;
+  extern vector<int> DIRS_2_Y;
+  extern vector<int> DIRS_3_Y;
+  extern vector<int> DIRS_4_Y;
+  extern vector<int> DIRS_5_Y;
+  extern vector<int> DIRS_6_Y;
+  extern vector<int> DIRS_7_Y;
+  extern vector<int> DIRS_8_Y;
+  extern vector<int> DIRS_9_Y;
+}
