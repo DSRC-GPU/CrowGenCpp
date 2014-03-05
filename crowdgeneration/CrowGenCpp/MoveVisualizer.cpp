9d4af680bad585d392a6a02c77e1ae0b3d337e55
Jesse Donkervliet <j.j.r.donkervliet@student.tudelft.nl>
2014-03-06 00:17:53 +0100

    MoveVisualizer structure setup.

--- crowdgeneration/CrowGenCpp/MoveVisualizer.cpp
+++ crowdgeneration/CrowGenCpp/MoveVisualizer.cpp
@@ -5,17 +5,32 @@ MoveVisualizer::MoveVisualizer()
 {
 }
 
-void MoveVisualizer::parse(string fin)
+void MoveVisualizer::visualize(vector<vector<Vertex>>& simulation, string fout)
+{
+  // FIXME Implement.
+  writeBasics();
+  for (size_t i = 0; i < simulation.size(); i++)
+  {
+    vector<Vertex> vertices = simulation.at(i);
+    for (size_t j = 0; j < vertices.size(); j++)
+    {
+      updateVertex(vertices.at(j));
+    }
+  }
+  flush();
+}
+
+void MoveVisualizer::writeBasics()
 {
   // FIXME Implement.
 }
 
-void MoveVisualizer::visualize(string fout)
+void MoveVisualizer::updateVertex(Vertex v)
 {
   // FIXME Implement.
 }
 
-void MoveVisualizer::visualize(string fin, string fout)
+void MoveVisualizer::flush()
 {
   // FIXME Implement.
 }
--- crowdgeneration/CrowGenCpp/MoveVisualizer.hpp
+++ crowdgeneration/CrowGenCpp/MoveVisualizer.hpp
@@ -3,18 +3,27 @@
 #define MOVEVISUALIZER_HPP
 
 #include <string>
+#include <vector>
+
+#include "tinyxml2-master/tinyxml2.h"
+#include "Vertex.hpp"
 
 using namespace std;
+using namespace tinyxml2;
 
 // The move visualizer parses a xml movement file and creates an animated svg
 // that visualizes the movement of all nodes.
 class MoveVisualizer
 {
+  private:
+    XMLDocument _doc;
+    void writeBasics();
+    void updateVertex(Vertex v);
+    void flush();
+
   public:
     MoveVisualizer();
-    void parse(string);
-    void visualize(string);
-    void visualize(string, string);
+    void visualize(vector<vector<Vertex>>&, string);
 };
 
 #endif
