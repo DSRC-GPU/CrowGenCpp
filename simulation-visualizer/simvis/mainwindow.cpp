cbdc7431f738f4c9cd8194a2295eb4975eab1ec7
Jesse Donkervliet <j.donkervliet@gmail.com>
2014-05-04 20:44:40 +0200

    Drawing a circle.

--- simulation-visualizer/simvis/mainwindow.cpp
+++ simulation-visualizer/simvis/mainwindow.cpp
@@ -1,14 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QGraphicsEllipseItem>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->graphicsView->setScene(new QGraphicsScene);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QGraphicsEllipseItem* e = new QGraphicsEllipseItem(0,0,10,10);
+    ui->graphicsView->scene()->addItem(e);
 }
