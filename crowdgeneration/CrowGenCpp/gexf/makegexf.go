aad3830324a0cf670c6be6688a9fe9d20f8952b8
Jesse Donkervliet <j.j.r.donkervliet@student.tudelft.nl>
2014-10-08 23:43:40 +0200

    Updated make gexf script.

--- crowdgeneration/CrowGenCpp/gexf/makegexf.go
+++ crowdgeneration/CrowGenCpp/gexf/makegexf.go
@@ -9,14 +9,22 @@ import(
 )
 
 func main() {
-  files, _ := filepath.Glob("../mapgen/map-10[0]*.xml")
-  fmt.Println(files)
-  for _,element := range files {
+  files, _ := filepath.Glob("../mapgen/map-*.xml")
+  // fmt.Println(files)
+  for i := 0; i < len(files); i++ {
+    element := files[i]
     out := strings.Split(element, "/")[2]
     out = strings.Split(out, ".")[0] + ".gexf"
+    var size int
+    fmt.Sscanf(out, "map-%d.gexf", &size)
+    if size > 10000 {
+      fmt.Println("Skipping gexf with", size, "nodes.")
+      continue
+    }
     fmt.Println(out)
-    command := exec.Command("prun", "-no-panda", "../src/cgc", "1", "-G", "-m",
-    element, "-g", out)
+
+    command := exec.Command("prun", "-no-panda", "../src/cgc", "1", "-G", "-t",
+    "50", "-m", element, "-g", out)
     command.Run()
   }
 }
